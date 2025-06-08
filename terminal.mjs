546c02f044490a3ca19b6de57271fa03eb4358d3
Iñaki Arango <arangoinaki@gmail.com>
2025-06-08 12:30:12 -0400

    chore: refactoring internal project structure

--- terminal.mjs
+++ terminal.mjs
@@ -1,11 +1,11 @@
-import { toggleFullscreen } from "./util/screens.js";
-import { registerHandlers } from "./util/ui.mjs";
+import { toggleFullscreen } from "./components/crt-monitor/screens.js";
+import { registerHandlers } from "./components/crt-monitor/ui.mjs";
 
 async function onLoad() {
 	// Check for query parameters in the URL, e.g. ?command=help&fullscreen=1
 	const urlParams = new URLSearchParams(window.location.search);
 	const fullscreen = urlParams.get("fullscreen");
-	const { on } = await import("./util/power.js");
+	const { on } = await import("./components/crt-monitor/power.js");
 
 	// Set up click event handlers for UI buttons
 	registerHandlers();
