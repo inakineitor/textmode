41f6399d8193da65899c7e13c2ee487ada986ad1
Iñaki Arango <arangoinaki@gmail.com>
2026-03-25 03:32:01 -0700

    screen glitch (#1)
    * fix attempt
    
    * fix
    
    * internal upgrades

--- components/terminal/_components/crt-monitor/power.js
+++ components/terminal/_components/crt-monitor/power.js
@@ -1,31 +1,37 @@
 import { click } from "../../../../utils/sound/index.js";
-import { boot } from "./screens.js";
 import { stopSpeaking } from "./speak.js";
 import pause from "./pause.js";
+
+/** @type {import('./screens.js').Screen} */
+let screen = null;
+
+export function setScreen(s) {
+	screen = s;
+}
 
 /** Turn on the terminal */
 async function on() {
 	click();
 	await power();
-	boot();
+	screen.boot();
 }
 
 /** Turn off the terminal */
 function off() {
 	click();
 	stopSpeaking();
+	screen.shutdown();
 	power(false);
 }
 
 async function power(on = true) {
-	// @FIXME use a single class on the #monitor to detect on/off
+	const monitor = document.getElementById("monitor");
 	document.querySelector("#slider").classList.toggle("on", on);
 	document.querySelector("#switch").checked = !on;
 	await pause(0.1);
 
-	document.getElementById("monitor").classList.toggle("turn-off", !on);
-	document.getElementById("monitor").classList.toggle("off", !on);
-	return;
+	monitor.classList.toggle("turn-off", !on);
+	monitor.classList.toggle("off", !on);
 }
 
 export { power, on, off };
